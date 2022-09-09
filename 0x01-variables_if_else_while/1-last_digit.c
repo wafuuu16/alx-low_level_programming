948d79385b88f092a80547060d49459a2d2fec1e
wafuuu16 <wjapheth9@gmail.com>
2022-09-09 09:45:18 -0700

    2. i sometimes suffer from insomnia. And when I cant fall asleep, I play what I call the alphabet game

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,8 @@
 
 /**
  * main - Entry point
- * Description: print status of value of n: is not less than 6, greater than and is zero
+ * Description: print status of value of n: 
+ * 		is not less than 6, greater than and is zero
  *
  * Return: Always 0 (Success)
  */
@@ -23,7 +24,7 @@ int main(void)
 		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
 	else if (digit == 0)
 		printf("Last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit !=0)
+	else if (digit < 6 && digit != 0)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
 
 	return (0);
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch  = 'a';
+	while (ch <= 'z')
+	{
+	putchar(ch);
+	++ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
