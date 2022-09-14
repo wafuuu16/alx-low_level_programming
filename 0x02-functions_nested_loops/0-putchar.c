27b9a8155ef2716d0b87c8868c19a52bb0e0a291
wafuuu16 <wjapheth9@gmail.com>
2022-09-13 22:42:10 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar followed by a new line
+ *
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
