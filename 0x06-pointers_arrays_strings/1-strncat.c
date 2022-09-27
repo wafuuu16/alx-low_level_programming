bb81f859ceab5c39d3f7db06ebb6114c6f5f8c9d
wafuuu16 <wjapheth9@gmail.com>
2022-09-27 06:48:27 -0700

    0-memset

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,17 @@
 char *_strncat(char *dest, char *src, int n)
 {
 		int count = 0, count2 = 0;
-			while (*(dest + count) != '\0')
-			{
-						count++;
-			}
-				while (count2 < n)
-				{
-					*(dest + count) = *(src + count2);
-					if (*(src + count2) == '\0')
-						break;
+		while (*(dest + count) != '\0')
+		{
 					count++;
-					count2++;
-				}
-				return (dest);
+		}
+		while (count2 < n)
+		{
+			*(dest + count) = *(src + count2);
+			if (*(src + count2) == '\0')
+				break;
+			count++;
+			count2++;
+		}
+		return (dest);
 }
